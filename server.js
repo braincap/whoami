55519a73ee6212af27eb1134739b486feb64a19f
Jai Tiwari <contactjaitiwari@gmail.com>
2017-04-07 20:57:20 -0400

    done

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api', (req, res) => {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var lang = req.headers["accept-language"];
+  var machine = req.headers["user-agent"];
+  res.send(JSON.stringify({
+    ipaddress: ip,
+    language: lang,
+    software: machine
+  }));
+});
+
+app.listen(port, () => console.log('Listening to port ' + port));
