2a6c7181a1dd8f69f4ef048e03f6e4a1337b73ef
braincap <contactjaitiwari@gmail.com>
2018-04-20 20:40:03 -0400

    updated uri

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api', (req, res) => {
+app.get('/api/whoami', (req, res) => {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var lang = req.headers["accept-language"];
   var machine = req.headers["user-agent"];
